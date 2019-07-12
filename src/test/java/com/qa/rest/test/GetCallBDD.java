9c04bd49a2bf4de09c7f16d6a60103e994cda227
Rakesh email = rakesh.gopari@anthem.com[core] <AF44976@ad.wellpoint.com>
2019-07-12 20:18:17 +0530

    first commit

---
+++ src/test/java/com/qa/rest/test/GetCallBDD.java
@@ -0,0 +1,33 @@
+package com.qa.rest.test;
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class GetCallBDD {
+	
+	
+	@Test
+	public void test_numberOfCircuitsFor2017_Season(){
+		
+//		given().
+//		when().
+//		then().
+//		assert().
+		
+		
+		
+		given().
+		when().get("http://ergast.com/api/f1/2017/circuits.json").
+		then().
+			assertThat().
+			statusCode(200).
+			and().
+			body("MRData.CircuitTable.Circuits.circuitId", hasSize(20)).
+			and().
+			header("content-length",equalTo("4551"));
+		
+	}
+
+}
